4b9249360a992cecea1a940e08cfc091654b138b
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-18 01:44:16 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 let breedArr =[];
-// fetch("https://dog.ceo/api/breeds/image/random/4")
-// .then(resp => resp.json())
-// .then(imgData => imgData.message.forEach(image => {
-//     displayImg(image)
-// }))
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(imgData => imgData.message.forEach(image => {
+    displayImg(image)
+}))
 
-// function displayImg(image){
-//     let display = document.getElementById('dog_image_container')
-//     let img = document.createElement('img')
-//     img.src = image
-//     display.appendChild(img)
-// }
+function displayImg(image){
+    let display = document.getElementById('dog_image_container')
+    let img = document.createElement('img')
+    img.src = image
+    display.appendChild(img)
+}
+
 fetch('https://dog.ceo/api/breeds/list/all')
 .then(resp =>resp.json())
 .then(breedData => {
