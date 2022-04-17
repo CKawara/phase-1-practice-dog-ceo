a4533d2f9cbaea3f59db5624481a8c894c91da4a
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-17 22:16:19 +0300

    fetch and display dog images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('%c HI', 'color: firebrick')
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(imgdata => imgdata.message.forEach(image => {
+    displayImg(image)
+}))
+
+function displayImg(image){
+    let display = document.getElementById('dog_image_container')
+    let img = document.createElement('img')
+    img.src = image
+    display.appendChild(img)
+}
